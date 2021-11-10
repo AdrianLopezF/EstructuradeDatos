26d23b285d33937582f6797703b6653978cf852d
AdrianLopezF <89925469+AdrianLopezF@users.noreply.github.com>
2021-11-09 18:18:08 -0600

    Add files via upload

---
+++ Clase3Nov.java
@@ -0,0 +1,12 @@
+package clase3nov;
+
+public class Clase3Nov {
+
+    public static void main(String[] args) {
+        
+       ListaEnlazada ven = new ListaEnlazada();
+       ven.ListaNodos();
+       
+    }
+    
+}
